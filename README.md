3ec02d1ecd38ac9971fbe2557d8f31e9ecf237d9
mynuong0301 <67038749+mynuong0301@users.noreply.github.com>
2022-04-30 08:45:57 +0700

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RecommendationSystem_ADMIN
- phân hệ Admin
+ 
+ 1 npm run build
+
+2 npm run dev
---
+++ src/pages/TestManager.vue
@@ -0,0 +1,28 @@
+<template>
+<!-- ============================================================== -->
+<!-- Start right Content here -->
+<!-- ============================================================== -->
+<div class="content-page">
+    <!-- Start content -->
+    <div class="content">
+        <div class="container-fluid">
+            <div class="page-title-box">
+                <div class="row align-items-center">
+                    <h4 class="page-title">Quản lý thông tin câu hỏi bài đánh giá định hướng nghề nghiệp</h4>
+                
+                </div>
+                <!-- end row -->
+            </div>
+            <!-- end page-title -->
+
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+
+export default ({
+    name: "TestManager",
+})
+</script>
